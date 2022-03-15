420cd3094da7460ff3106144259bc42d3f2c1e0b
MarieCHessler <marie@hessler.se>
2022-03-15 16:41:28 +0000

    Initial commit

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("h2").addClass("underline"); // Underlines all H2 elements
+    //  $("h2").css("text-decoration", "underline"); // Underlines all H2 elements, without class created in CSS
+    // $("h2").removeClass("underline"); // Removes underline from all H2 elements
+
+    $("ul").addClass("border"); // Adds border to all lists
+    //  $("ul").css("border", "solid 1px #ccc"); // Adds border to all lists, without class created in CSS
+    // $("ul").removeClass("border"); // Removes border from all lists
+});
